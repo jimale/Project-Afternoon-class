cddfcea6e3f902ead507df2323c0e4b12228d34e
Jimale <jimcalecabdi@gmail.com>
2021-03-16 17:49:37 +0300

    Added spinner listener

--- app/src/main/java/com/tiriig/fistappafternoon/DisplayActivity.kt
+++ app/src/main/java/com/tiriig/fistappafternoon/DisplayActivity.kt
@@ -8,18 +8,34 @@ import androidx.appcompat.app.AppCompatActivity
 class DisplayActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
+    private lateinit var language : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
         val spinner = findViewById<Spinner>(R.id.mySpinner)
+        val myText = findViewById<TextView>(R.id.displayText)
 
         val languages = arrayOf("Select language","PHP","Java","Kotlin")
+
         val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,languages)
 
         spinner.adapter = adapter
 
+        //click listener of the spinner
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                myText.text = languages[position]
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
 
 //        textView = findViewById(R.id.displayText)
 //
