5e310df67e92d94d1807523712df049dff75736b
Jimale <jimcalecabdi@gmail.com>
2021-03-23 17:47:41 +0300

    Reading data from Database

--- app/src/main/java/com/tiriig/fistappafternoon/MainActivity.kt
+++ app/src/main/java/com/tiriig/fistappafternoon/MainActivity.kt
@@ -15,15 +15,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mybutton: Button
     private lateinit var myTextView: TextView
+    private lateinit var name: TextView
+    private lateinit var phone: TextView
     private lateinit var myEditText : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myTextView = findViewById(R.id.name)
         mybutton = findViewById(R.id.myButton)
         myEditText = findViewById(R.id.myEditText)
+
+
+        name = findViewById(R.id.name)
+        phone = findViewById(R.id.phone)
+
+
 
 
         mybutton.setOnClickListener {
@@ -38,16 +45,56 @@ class MainActivity : AppCompatActivity() {
 //            ab.putExtra("name",myEditText.text.toString())
 //            startActivity(ab)
 
+            Thread{
+
+                val db = Room.databaseBuilder(applicationContext,
+                        MyDatabase::class.java,
+                        "mydatabase").build()
+
+                val userDao  = db.userDao()
+
+                //INSERTING SINGLE USER TO THE DATABASE
+//                val user = User(1,"Mohamed",47838392)
+//                userDao.insertUser(user)
+
+                //INSERTING MULTIPLE USERS TO THE DATABASE
+                val user = ArrayList<User>()
+                user.add(User(2,"Jamac",232423))
+                user.add(User(3,"Cali",4353534))
+                user.add(User(4,"Xasan",8534354))
+                user.add(User(5,"Casha",5439845))
+
+                userDao.insertUsers(user)
+
+            }.start()
+
+        }
+
+        val read = findViewById<Button>(R.id.read)
+
+        read.setOnClickListener {
+            readUser()
+        }
+    }
+
+
+    fun readUser(){
+        Thread{
+
             val db = Room.databaseBuilder(applicationContext,
                     MyDatabase::class.java,
                     "mydatabase").build()
 
             val userDao  = db.userDao()
 
-            val user = User(1,"Mohamed",47838392)
+            val user = userDao.getUser(5)
 
-            userDao.insertUser(user)
-        }
+            runOnUiThread(Runnable {
+                name.text = user.name
+                phone.text = user.phone.toString()
+            })
+
+        }.start()
     }
 }
 
--- app/src/main/java/com/tiriig/fistappafternoon/UserDao.kt
+++ app/src/main/java/com/tiriig/fistappafternoon/UserDao.kt
@@ -1,6 +1,7 @@
 package com.tiriig.fistappafternoon
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
@@ -8,5 +9,12 @@ interface UserDao {
     @Query("SELECT * FROM user")
     fun getAllUsers(): List<User>
 
+    @Insert
     fun insertUser(user: User)
+
+    @Insert
+    fun insertUsers(user: List<User>)
+
+    @Query("SELECT * FROM user Where id IN (:userId)")
+    fun getUser(userId: Int): User
 }
