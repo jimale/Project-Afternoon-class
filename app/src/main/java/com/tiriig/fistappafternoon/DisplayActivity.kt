e55c389f794ae4b235d0465a120008928386e5bb
Jimale <jimcalecabdi@gmail.com>
2021-06-15 17:42:59 +0300

    Added RecyclerView

--- app/src/main/java/com/tiriig/fistappafternoon/DisplayActivity.kt
+++ app/src/main/java/com/tiriig/fistappafternoon/DisplayActivity.kt
@@ -4,44 +4,32 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class DisplayActivity : AppCompatActivity() {
 
-    private lateinit var textView: TextView
-    private lateinit var language : String
+    private lateinit var recyclerView: RecyclerView
+
+    private val adapter = DisplayAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
-        val spinner = findViewById<Spinner>(R.id.mySpinner)
-        val myText = findViewById<TextView>(R.id.displayText)
+        setRecyclerView()
+    }
 
-        val languages = arrayOf("Select language","PHP","Java","Kotlin")
+    private fun setRecyclerView() {
+        val data = ArrayList<User>()
+        data.add(User(1,"Ahmed ALi Jama","5754848","ahmed@gmail.com"))
+        data.add(User(2,"Mohamed ALi Jama","435938","mohamed@gmail.com"))
+        data.add(User(3,"Jama ALi Jama","837393","jama@gmail.com"))
 
-        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,languages)
 
-        spinner.adapter = adapter
 
-        //click listener of the spinner
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
-                myText.text = languages[position]
-
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-        }
-
-//        textView = findViewById(R.id.displayText)
-//
-//        textView.text = intent.getStringExtra("name")
-//
-//        title = intent.getStringExtra("name")
+        recyclerView = findViewById(R.id.displayRecyclerView)
+        adapter.submitList(data)
+        recyclerView.adapter = adapter
     }
 
 }
---
+++ app/src/main/java/com/tiriig/fistappafternoon/DisplayAdapter.kt
@@ -0,0 +1,46 @@
+package com.tiriig.fistappafternoon
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class DisplayAdapter : ListAdapter<User,DisplayAdapter.ViewHolder>(DisplayDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DisplayAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DisplayAdapter.ViewHolder, position: Int) {
+        val data = getItem(position)
+        holder.bind(data)
+    }
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+        val name : TextView = itemView.findViewById(R.id.name)
+        val phone : TextView = itemView.findViewById(R.id.phone)
+        val email : TextView = itemView.findViewById(R.id.email)
+
+        fun bind(user: User){
+            name.text = user.fullName
+            phone.text = user.phoneNumber
+            email.text = user.email
+        }
+    }
+
+    class DisplayDiffCallback : DiffUtil.ItemCallback<User>(){
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+}
